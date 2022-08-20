f7d7769e7a02618388539e2a0ad6dfcf508858ef
Jay Indino <45135991+jaydesigns@users.noreply.github.com>
2022-08-19 19:35:24 -0600

    Mispelling correction

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import axios from 'axios'
 
+const App =()=> {
 
-function App() {
+  axios
+  .get('https://data.mongodb-api.com/app/data-pucta/endpoint/data/v1')
+  .then(response => {
+    console.log(response)
+  })
+
   return (
     <>
     <h1>Clifora</h1>
-    <h1>Th best in the world woohoo!!!!!</h1>
+    <h1>The best in the world woohoo!!!!!</h1>
     </>
   );
 }
